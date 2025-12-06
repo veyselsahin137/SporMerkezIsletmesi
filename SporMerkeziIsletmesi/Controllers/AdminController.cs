e269b22dfe354212c9fbf934f22b8eaf490a319e
Melih-do <17erozmelihhh@gmail.com>
2025-12-06 22:49:01 +0300

    AdminController.cs oluşturdum bu şekilde tüm controllerlara admin yetkisi ola girebilir.

---
+++ SporMerkeziIsletmesi/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SporMerkeziIsletmesi.Controllers
+{
+    // Bu controller'a sadece Admin rolündeki kullanıcılar erişebilsin
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
