fbf549623e46bf8b32b9c9afca2d20d3d185a4a7
Melih-do <17erozmelihhh@gmail.com>
2025-12-09 20:14:29 +0300

    AntrenorApiController isimli bir API controller oluşturdum. Bu şekilde tüm antrenörleri listeleyebilir ve belli hizmeti veren antrenörleri LINQ ile filtreleyebilir halde. REST API ve LINQ şartı sağlanıyor

---
+++ SporMerkeziIsletmesi/Controllers/AntrenorApiController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SporMerkeziIsletmesi.Data;
+using SporMerkeziIsletmesi.Models;
+
+namespace SporMerkeziIsletmesi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AntrenorApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AntrenorApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/AntrenorApi
+        // Tüm antrenörleri JSON olarak döndürür
+        [HttpGet]
+        [AllowAnonymous]   // İstersen bunu kaldırıp sadece login olanlara açabilirsin
+        public async Task<IActionResult> GetAntrenorler()
+        {
+            var antrenorler = await _context.Antrenorler
+                .Select(a => new
+                {
+                    a.AntrenorID,
+                    a.Ad,
+                    a.Soyad,
+                    AdSoyad = a.Ad + " " + a.Soyad,
+                    a.UzmanlikAlani,
+                    a.Telefon,
+                    a.Email,
+                    a.AktifMi
+                })
+                .ToListAsync();
+
+            return Ok(antrenorler);
+        }
+
+        // GET: api/AntrenorApi/ByHizmet/3
+        // Belirli bir hizmeti verebilen AKTİF antrenörleri LINQ ile filtreler
+        [HttpGet("ByHizmet/{hizmetId:int}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetAntrenorlerByHizmet(int hizmetId)
+        {
+            var sonuc = await _context.AntrenorHizmetler
+                .Include(ah => ah.Antrenor)
+                .Include(ah => ah.Hizmet)
+                .Where(ah => ah.HizmetID == hizmetId && ah.Antrenor.AktifMi)
+                .Select(ah => new
+                {
+                    ah.Antrenor.AntrenorID,
+                    Ad = ah.Antrenor.Ad,
+                    Soyad = ah.Antrenor.Soyad,
+                    AdSoyad = ah.Antrenor.Ad + " " + ah.Antrenor.Soyad,
+                    UzmanlikAlani = ah.Antrenor.UzmanlikAlani,
+
+                    HizmetID = ah.HizmetID,
+                    HizmetAdi = ah.Hizmet.HizmetAdi,
+                    SureDakika = ah.Hizmet.SureDakika,
+                    Ucret = ah.Hizmet.Ucret
+                })
+                .ToListAsync();
+
+            return Ok(sonuc);
+        }
+    }
+}
